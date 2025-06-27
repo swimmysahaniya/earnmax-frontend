5ac407e12e80adc7bbc16ed0730357fb0ceac688
Ramagyaweb2021 <websitedeveloper@ramagyaschool.com>
2025-06-27 17:05:51 +0530

    PWA

--- includes/footer.php
+++ includes/footer.php
@@ -19,6 +19,13 @@
 
 </script> -->
 
+<script>
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(reg => console.log("Service Worker registered:", reg))
+      .catch(err => console.log("SW registration failed:", err));
+  }
+</script>
 </body>
 
 </html>
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("earnmax-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.php",
+        "/style.css",
+        "/login.css",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
