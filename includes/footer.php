9a01b8d6b55dafd0d9366bcb959d74e14ac1352f
Ramagyaweb2021 <websitedeveloper@ramagyaschool.com>
2025-06-28 18:45:32 +0530

    remove manifest and service-worker

--- includes/footer.php
+++ includes/footer.php
@@ -18,28 +18,6 @@
   }, false);
 
 </script> -->
-
-<!-- <script>
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/service-worker.js")
-      .then(reg => console.log("Service Worker registered:", reg))
-      .catch(err => console.log("SW registration failed:", err));
-  }
-</script> -->
-
-<script>
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(function (registration) {
-          console.log('✅ Service Worker registered with scope:', registration.scope);
-        })
-        .catch(function (error) {
-          console.error('❌ Service Worker registration failed:', error);
-        });
-    });
-  }
-</script>
 </body>
 
 </html>
--- includes/head.php
+++ includes/head.php
@@ -5,9 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="images/logo.png">
-  <link rel="manifest" href="/manifest.json">
-  <meta name="theme-color" content="#0f4c81">
-  <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
   <title>The Earn Max</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="style.css" rel="stylesheet">
